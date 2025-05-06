1f4ac8421d7a370c4841a9ec8b5fc4aeb96e4c9d
Siavosh Jazmi <s.jazmi.matlabsite@gmail.com>
2025-05-06 23:07:28 +0330

    chore:add account_name

--- src/Models/User.php
+++ src/Models/User.php
@@ -32,6 +32,7 @@ class User extends Authenticatable
     protected $fillable = [
         'uuid',
         'account_uuid',
+        'account_name',
         'email',
         'phone',
         'username',
